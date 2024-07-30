89dca5851424830857214ba44a38929f899aac30
subahan ali <subahanislam523@gmail.com>
2024-07-30 00:30:43 -0600

    Implement Cover With With Blur Parallax Using React Parallax

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,14 +1,14 @@
 import { Helmet } from "react-helmet-async";
 import MenuCover from "../../Routes/Shared/MenuCover/MenuCover";
 import PopularMenu from "../PopularMenu";
-
+import img from '../../assets/menu/banner3.jpg'
 const Menu = () => {
   return (
     <div>
       <Helmet>
         <title>Bistro Restaurant | menu</title>
       </Helmet>
-      <MenuCover></MenuCover>
+      <MenuCover img={img} title="OUR MENU"></MenuCover>
       <PopularMenu></PopularMenu>
     </div>
   );
--- src/Routes/Shared/MenuCover/MenuCover.jsx
+++ src/Routes/Shared/MenuCover/MenuCover.jsx
@@ -1,24 +1,26 @@
 
-import bgImage from '../../../assets/menu/banner3.jpg'
-const MenuCover = () => {
-    return (
-      <div
-        className="hero h-[700px]  bg-fixed mb-10"
-        style={{
-          backgroundImage: `url(${bgImage})`,
-        }}
-      >
+import { Parallax } from "react-parallax";
+const MenuCover = ({ img, title }) => {
+  return (
+    <Parallax
+      blur={{ min: -15, max: 15 }}
+      bgImage={img}
+      bgImageAlt="the menu"
+      strength={-200}
+    >
+      <div className="hero h-[700px]">
         <div className="hero-overlay  bg-opacity-60"></div>
         <div className="hero-content text-neutral-content text-center">
           <div className="max-w-screen-sm p-8  bg-black bg-opacity-40 ">
             <h1 className="mb-5 text-5xl w-[620px]  font-bold text-gray-300 uppercase">
-              OUR MENU
+              {title}
             </h1>
-            <p className=''>Would you like to try a dish?</p>
+            <p className="">Would you like to try a dish?</p>
           </div>
         </div>
       </div>
-    );
+    </Parallax>
+  );
 };
 
 export default MenuCover;

0e30802859b6ce959e7a8abed8598988412a34f0
subahan ali <subahanislam523@gmail.com>
2024-08-13 17:58:18 +0600

    Upload image to image hosting server imgbb and get image url

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,16 +1,26 @@
 import { useForm } from "react-hook-form";
 import SectionTitle from "../../../components/SectionTitle";
 import { FaUtensils } from "react-icons/fa";
-
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+const image_hosting_key = import.meta.env.VITE_IMAGE_UPLOAD_TOKEN;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 const AddItems = () => {
+  const axiosPublic = useAxiosPublic();
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     console.log(data);
+    const imageFile = { image: data.image[0] };
+    // image upload to image an then get url
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        "content-type": "multipart/form-data",
+      },
+    });
+    console.log(res.data);
   };
 
   return (
@@ -46,10 +56,11 @@ const AddItems = () => {
                 </div>
               </label>
               <select
+                defaultValue="default"
                 {...register("category", { required: true })}
                 className="select select-bordered w-full "
               >
-                <option disabled selected>
+                <option disabled value="default">
                   Select a category
                 </option>
                 <option value="salad">Salad</option>
